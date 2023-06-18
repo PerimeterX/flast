523742add35b3505b5726c7aac6ea6478a968ffd
Ben Baryo <19845603+ctrl-escp@users.noreply.github.com>
2023-06-18 22:02:07 +0300

    Modularize: break generateFlatAST into smaller pieces. Refactor getParentKey to improve performance

--- src/flast.js
+++ src/flast.js
@@ -15,19 +15,25 @@ function parseCode(inputCode, opts = {}) {
 	return parse(inputCode, {ecmaVersion, comment: true, range: true, ...opts});
 }
 
+const excludedParentKeys = [
+	'type', 'start', 'end', 'range', 'sourceType', 'comments', 'srcClosure', 'nodeId',
+	'childNodes', 'parentNode', 'parentKey', 'scope',
+];
+
 /**
  * Return the key the child node is assigned in the parent node if applicable; null otherwise.
- * @param {ASTNode} parent
- * @param {number} targetChildNodeId
+ * @param {ASTNode} node
  * @returns {string|null}
  */
-function getParentKey(parent, targetChildNodeId) {
-	if (parent) {
-		for (const key of Object.keys(parent)) {
-			if (parent[key]?.nodeId === targetChildNodeId) return key;
-			else if (Array.isArray(parent[key])) {
-				for (const item of (parent[key] || [])) {
-					if (item?.nodeId === targetChildNodeId) return key;
+function getParentKey(node) {
+	if (node.parentNode) {
+		const keys = Object.keys(node.parentNode);
+		for (let i = 0; i < keys.length; i++) {
+			if (excludedParentKeys.includes(keys[i])) continue;
+			if (node.parentNode[keys[i]] === node) return keys[i];
+			if (Array.isArray(node.parentNode[keys[i]])) {
+				for (let j = 0; j < node.parentNode[keys[i]]?.length; j++) {
+					if (node.parentNode[keys[i]][j] === node) return keys[i];
 				}
 			}
 		}
@@ -36,9 +42,9 @@ function getParentKey(parent, targetChildNodeId) {
 }
 
 const generateFlatASTDefaultOptions = {
-	// If false, include only original node with nodeId, without any further details
+	// If false, do not include any scope details
 	detailed: true,
-	// If false, do not include node src. Only available when `detailed` option is true
+	// If false, do not include node src
 	includeSrc: true,
 	// Retry to parse the code with sourceType: 'script' if 'module' failed with 'strict' error message
 	alernateSourceTypeOnFailure: true,
@@ -64,88 +70,12 @@ function createSrcClosure(src) {
  */
 function generateFlatAST(inputCode, opts = {}) {
 	opts = { ...generateFlatASTDefaultOptions, ...opts };
-	const parseOpts = opts.parseOpts || {};
-	let rootNode;
-	try {
-		rootNode = parseCode(inputCode, parseOpts);
-	} catch (e) {
-		if (opts.alernateSourceTypeOnFailure && e.message.includes('in strict mode')) rootNode = parseCode(inputCode, {...parseOpts, sourceType: 'script'});
+	const rootNode = generateRootNode(inputCode, opts);
+	const tree = extractNodesFromRoot(rootNode, opts);
+	const sm = initScopeManager(rootNode);
+	if (opts.detailed) {
+		for (let i = 0; i < tree.length; i++) injectScopeToNode(tree[i], sm);
 	}
-	let scopeManager;
-	let srcClosure;
-	try {
-		if (opts.detailed) { // noinspection JSCheckFunctionSignatures
-			scopeManager = analyze(rootNode, {
-				optimistic: true,
-				ecmaVersion,
-				sourceType});
-			if (opts.includeSrc) srcClosure = createSrcClosure(inputCode);
-		}
-	} catch {}
-	const tree = [];
-	let nodeId = 0;
-	let scopeId = 0;
-	estraverse.traverse(rootNode, {
-		/**
-		 * @param {ASTNode} node
-		 * @param {ASTNode} parentNode
-		 */
-		enter(node, parentNode) {
-			node.nodeId = nodeId++;
-			if (opts.detailed) {
-				if (opts.includeSrc) Object.defineProperty(node, 'src', {
-					get() { return srcClosure(node.range[0], node.range[1]);},
-				});
-				node.childNodes = [];
-				node.parentNode = parentNode;
-				node.parentKey = parentNode ? getParentKey(parentNode, node.nodeId) : '';
-
-				// Keep track of the node's lineage
-				if (parentNode) node.lineage = [...parentNode?.lineage || [], parentNode.nodeId];
-				// Acquire scope
-				node.scope = scopeManager.acquire(node);
-				if (!node.scope) node.scope = node.parentNode.scope;
-				else if (node.scope.type.includes('-name') && node.scope?.childScopes?.length === 1) node.scope = node.scope.childScopes[0];
-				if (node.scope.scopeId === undefined) node.scope.scopeId = scopeId++;
-				if (parentNode) parentNode.childNodes.push(node);
-				if (node.type === 'Identifier') {
-					// Track references and declarations
-					// Prevent assigning declNode to member expression properties or object keys
-					if (!(['property', 'key'].includes(node.parentKey) && !parentNode.computed)) {
-						const variables = node.scope.variables.filter(n => n.name === node.name);
-						const isDeclaration = variables?.length && variables[0].identifiers.filter(n => n.nodeId === node.nodeId).length;
-						if (isDeclaration) node.references = node.references || [];
-						else if (!(node.parentKey === 'id' && node.parentNode.type === 'FunctionDeclaration'))  {
-							// Find declaration by finding the closest declaration of the same name.
-							let decls = [];
-							if (variables?.length) decls = variables.filter(n => n.name === node.name)[0].identifiers;
-							else {
-								const scopeReferences = node.scope.references.filter(n => n.identifier.name === node.name);
-								if (scopeReferences.length) decls = scopeReferences[0].resolved?.identifiers || [];
-							}
-							let declNode = decls[0];
-							if (decls.length > 1) {   // TODO: Defer setting declaration and references
-								let commonAncestors = node.lineage.reduce((t, c) => declNode.lineage?.includes(c) ? ++t : t, 0);
-								decls.slice(1).forEach(n => {
-									const ca = node.lineage.reduce((t, c) => n.lineage?.includes(c) ? ++t : t, 0);
-									if (ca > commonAncestors) {
-										commonAncestors = ca;
-										declNode = n;
-									}
-								});
-							}
-							if (declNode) {
-								if (!declNode.references) declNode.references = [];
-								declNode.references.push(node);
-								node.declNode = declNode;
-							}
-						}
-					}
-				}
-			}
-			tree.push(node);
-		},
-	});
 	return tree;
 }
 
@@ -171,9 +101,109 @@ function generateCode(rootNode, opts = {}) {
 	return generate(rootNode, { ...generateCodeDefaultOptions, ...opts });
 }
 
+function generateRootNode(inputCode, opts = {}) {
+	opts = { ...generateFlatASTDefaultOptions, ...opts };
+	const parseOpts = opts.parseOpts || {};
+	let rootNode;
+	try {
+		rootNode = parseCode(inputCode, parseOpts);
+		if (opts.includeSrc) rootNode.srcClosure = createSrcClosure(inputCode);
+	} catch (e) {
+		if (opts.alernateSourceTypeOnFailure && e.message.includes('in strict mode')) rootNode = parseCode(inputCode, {...parseOpts, sourceType: 'script'});
+	}
+	return rootNode;
+}
+
+function extractNodesFromRoot(rootNode, opts) {
+	opts = { ...generateFlatASTDefaultOptions, ...opts };
+	const tree = [];
+	let nodeId = 0;
+
+	estraverse.traverse(rootNode, {
+		/**
+		 * @param {ASTNode} node
+		 * @param {ASTNode} parentNode
+		 */
+		enter(node, parentNode) {
+			tree.push(node);
+			node.nodeId = nodeId++;
+			node.childNodes = [];
+			node.parentNode = parentNode;
+			// Keep track of the node's lineage
+			node.parentKey = parentNode ? getParentKey(node) : '';
+			if (opts.includeSrc) Object.defineProperty(node, 'src', {
+				get() { return rootNode.srcClosure(node.range[0], node.range[1]);},
+			});
+		}
+	});
+	return tree;
+}
+
+function initScopeManager(rootNode) {
+	// noinspection JSCheckFunctionSignatures
+	return analyze(rootNode, {
+		optimistic: true,
+		ecmaVersion,
+		sourceType});
+}
+
+/**
+ *
+ * @param {ASTNode} node
+ * @param {ScopeManager} sm
+ */
+function injectScopeToNode(node, sm) {
+	let parentNode = node.parentNode;
+	// Acquire scope
+	node.scope = sm.acquire(node);
+	if (!node.scope) node.scope = node.parentNode.scope;
+	else if (node.scope.type.includes('-name') && node.scope?.childScopes?.length === 1) node.scope = node.scope.childScopes[0];
+	if (node.scope.scopeId === undefined) node.scope.scopeId = node.scope.block.nodeId;
+	if (parentNode) {
+		node.lineage = [...parentNode?.lineage || [], parentNode.nodeId];
+		parentNode.childNodes.push(node);
+	}
+	if (node.type === 'Identifier') {
+		// Track references and declarations
+		// Prevent assigning declNode to member expression properties or object keys
+		if (!(['property', 'key'].includes(node.parentKey) && !parentNode.computed)) {
+			const variables = node.scope.variables.filter(n => n.name === node.name);
+			const isDeclaration = variables?.length && variables[0].identifiers.filter(n => n.nodeId === node.nodeId).length;
+			if (isDeclaration) node.references = node.references || [];
+			else if (!(node.parentKey === 'id' && node.parentNode.type === 'FunctionDeclaration'))  {
+				// Find declaration by finding the closest declaration of the same name.
+				let decls = [];
+				if (variables?.length) decls = variables.filter(n => n.name === node.name)[0].identifiers;
+				else {
+					const scopeReferences = node.scope.references.filter(n => n.identifier.name === node.name);
+					if (scopeReferences.length) decls = scopeReferences[0].resolved?.identifiers || [];
+				}
+				let declNode = decls[0];
+				if (decls.length > 1) {   // TODO: Defer setting declaration and references
+					let commonAncestors = node.lineage.reduce((t, c) => declNode.lineage?.includes(c) ? ++t : t, 0);
+					decls.slice(1).forEach(n => {
+						const ca = node.lineage.reduce((t, c) => n.lineage?.includes(c) ? ++t : t, 0);
+						if (ca > commonAncestors) {
+							commonAncestors = ca;
+							declNode = n;
+						}
+					});
+				}
+				if (declNode) {
+					if (!declNode.references) declNode.references = [];
+					declNode.references.push(node);
+					node.declNode = declNode;
+				}
+			}
+		}
+	}
+}
+
 module.exports = {
 	estraverse,
+	extractNodesFromRoot,
 	generateCode,
 	generateFlatAST,
+	generateRootNode,
 	parseCode,
 };
