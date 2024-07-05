19b759abda9502cca1863d14ba8fa961dc30e5a4
Ben Baryo <19845603+ctrl-escp@users.noreply.github.com>
2024-07-05 19:35:48 +0300

    Add utils: - applyIteratively - treeModifier - logger

---
+++ src/utils/applyIteratively.js
@@ -0,0 +1,65 @@
+const {Arborist} = require('../arborist');
+const logger = require('./logger');
+const {createHash} = require('node:crypto');
+
+const generateHash = str => createHash('sha256').update(str).digest('hex');
+
+
+/**
+ * Apply functions to modify the script repeatedly until they are no long effective or the max number of iterations is reached.
+ * @param {string} script The target script to run the functions on.
+ * @param {function[]} funcs
+ * @param {number?} maxIterations (optional) Stop the loop after this many iterations at most.
+ * @return {string} The possibly modified script.
+ */
+function runLoop(script, funcs, maxIterations = 500) {
+	let scriptSnapshot = '';
+	let currentIteration = 0;
+	let changesCounter = 0;
+	let iterationsCounter = 0;
+	try {
+		let scriptHash = generateHash(script);
+		let arborist = new Arborist(script);
+		while (arborist.ast?.length && scriptSnapshot !== script && currentIteration < maxIterations) {
+			const iterationStartTime = Date.now();
+			scriptSnapshot = script;
+			// Mark each node with the script hash to distinguish cache of different scripts.
+			for (let i = 0; i < arborist.ast.length; i++) arborist.ast[i].scriptHash = scriptHash;
+			for (let i = 0; i <  funcs.length; i++) {
+				const func = funcs[i];
+				const funcStartTime = +new Date();
+				try {
+					logger.debug(`\t[!] Running ${func.name}...`);
+					arborist = func(arborist);
+					if (!arborist.ast?.length) break;
+					// If the hash doesn't exist it means the Arborist was replaced
+					const numberOfNewChanges = arborist.getNumberOfChanges() + +!arborist.ast[0].scriptHash;
+					if (numberOfNewChanges) {
+						changesCounter += numberOfNewChanges;
+						logger.log(`\t[+] ${func.name} applying ${numberOfNewChanges} new changes!`);
+						arborist.applyChanges();
+						script = arborist.script;
+						scriptHash = generateHash(script);
+						for (let j = 0; j < arborist.ast.length; j++) arborist.ast[j].scriptHash = scriptHash;
+					}
+				} catch (e) {
+					logger.error(`[-] Error in ${func.name} (iteration #${iterationsCounter}): ${e}\n${e.stack}`);
+				} finally {
+					logger.debug(`\t\t[!] Running ${func.name} completed in ` +
+              `${((+new Date() - funcStartTime) / 1000).toFixed(3)} seconds`);
+				}
+			}
+			++currentIteration;
+			++iterationsCounter;
+			logger.log(`[+] ==> Iteartion #${iterationsCounter} completed in ${(Date.now() - iterationStartTime) / 1000} seconds` +
+          ` with ${changesCounter ? changesCounter : 'no'} changes (${arborist.ast?.length || '???'} nodes)`);
+			changesCounter =  0;
+		}
+		if (changesCounter) script = arborist.script;
+	} catch (e) {
+		logger.error(`[-] Error on iteration #${iterationsCounter}: ${e}\n${e.stack}`);
+	}
+	return script;
+}
+
+module.exports = runLoop;
